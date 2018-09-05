45edac89f42192c2cacf351d84d57ef2bd1ce871
Shayan Sharifi <31035943+shayanro@users.noreply.github.com>
2018-09-05 12:39:09 +0430

    Nature paper Architec is ready

--- Neural_Nets/CNN.py
+++ Neural_Nets/CNN.py
@@ -9,7 +9,7 @@ import time
 
 name = "ecg-cnn-{}".format(int(time.time()))
 
-def generate_Model(optimizer='adam', loss='categorical_crossentropy', activation=tf.nn.relu):
+def generate_Model(optimizer='adam', loss='categorical_crossentropy'):
     model.add(Conv1D(256, 3, input_shape=X.shape[1:]))
     model.add(Activation('relu'))
     model.add(MaxPooling1D(pool_size=3))
@@ -27,20 +27,22 @@ def generate_Model(optimizer='adam', loss='categorical_crossentropy', activation
     model.add(Activation('sigmoid'))
 
 
+    tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))   # initialize Tensorboard
+
     model.compile(optimizer=optimizer,  # Good default optimizer to start with
                   loss=loss,
                   # how will we calculate our "error." Neural network aims to minimize loss.
                   metrics=['accuracy'])  # what to track
-    return model
+    return model, tensorboard
 
 def main():
     X, y = data(normalize=True)
-    model = generateModel()
-    model.fit(x_train, y_train, epochs=10)  # train the model, 10 epochs
-    val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
-    print(val_loss)  # model's loss (error)
-    print(val_acc)  # model's accuracy
-
+    model, tensorboard = generateModel()
+    model.fit(X, y,
+              batch_size=32,
+              validation_split=0.1,
+              epochs=10,
+              callbacks=[tensorboard])  # train the model, 10 epochs with callback for tensorboard
 
 if __name__ == '__main__':
     main()
