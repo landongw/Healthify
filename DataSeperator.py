9a9e51fba383b653dc9cd5a9d9c306be8545dc31
Behrad Moniri <bemoniri@live.com>
2018-09-05 22:06:48 +0430

    minor bug fix

--- DataSeperator.py
+++ DataSeperator.py
@@ -13,16 +13,14 @@ with open("Label/data.csv") as tsv:
     for line in csv.reader(tsv):
         dictionary[int(float(line[0]))] = line[3]
 
-#patients = os.listdir(location)  # List of patients
-#patients = sorted(patients)
-patients = ['patient100']
+patients = os.listdir(location)  # List of patients
+patients = sorted(patients)
+# patients = ['patient100']
 raw_signal = []
 Signals = []
 Labels = []
 
-
-
-for patient in patients[0:]:  # for each patient (avoid RECORDS.txt)
+for patient in patients[1:]:  # for each patient (avoid RECORDS.txt)
     print(patient)
     files = os.listdir(location + patient)  # load the patient's files
     for file in files:  # for each file
@@ -38,20 +36,20 @@ for patient in patients[0:]:  # for each patient (avoid RECORDS.txt)
             h = sig.firwin(101, [10, 50], width=None, window='hamming', pass_zero=False, scale=True, fs=fs)
             ECG_filtered = sig.fftconvolve(h, raw_signal)  # filter
             ECG_filtered = ECG_filtered[0::10]  # Down sample
-            fs = fs/10
+            fs = fs / 10
 
             # Write in database
-            size = int(len(ECG_filtered) / (10*fs))
+            size = int(len(ECG_filtered) / (10 * fs))
 
             for i in range(size):
-                Signals.append(ECG_filtered[int(i * fs * 10):   int(fs * (i + 1) * 10)]) # Patients Signal
+                Signals.append(ECG_filtered[int(i * fs * 10):   int(fs * (i + 1) * 10)])  # Patients Signal
                 Labels.append(dictionary[int(patient[7:])])  # Patients Label
 
                 with open("db.csv", "a+") as file:
                     writer = csv.writer(file)
 
-                    writer.writerow([dictionary[int(patient[7:])], ECG_filtered[int(i * fs * 10):   int(fs * (i + 1) * 10)]])
-
+                    writer.writerow(
+                        [dictionary[int(patient[7:])], ECG_filtered[int(i * fs * 10):   int(fs * (i + 1) * 10)]])
 
 print(Signals)
 print(Labels)
