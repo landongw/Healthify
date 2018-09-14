f2398473392ba151798d235bf592e80a54e082b2
Behrad Moniri <bemoniri@live.com>
2018-09-14 16:35:07 +0430

    PCA, SVM and Cross-Validating

--- Feature_Reduction.py
+++ Feature_Reduction.py
@@ -1,45 +1,54 @@
-from sklearn.decomposition import PCA
 import csv
-import os
-import feature
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 import numpy as np
-
-location = '/media/bemoniri/d08ff677-13e1-4052-b55d-6337b6f8c634/PTBDB-DataBase/'  # Database location
-header_data = './Label/data.csv'  # Labels location
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
 
 dictionary = {}  # Patient's Disease Dictionary
-with open("Label/data.csv") as tsv:
-    for line in csv.reader(tsv):
-        dictionary[int(float(line[0]))] = line[3]
-
-patients = os.listdir(location)  # List of patients
-patients = sorted(patients)
 Labels = []
-Feature = []
+Signals = []
+kappa = 0
+with open("feature_list.txt") as tsv:
+    for line in csv.reader(tsv):
+        Labels.append(line[0])
+        Signals.append([float(x) for x in line[1:-1]])
 
-error = 0
-for patient in patients:  # for each patient (avoid RECORDS.txt)
-    print(patient)
-    if os.listdir(location + patient):
-        files = os.listdir(location + patient)  # load the patient's files
-        for file in files:  # for each file
-            if file.find("Data") != -1:  # if it is not a header file
-                print(file)
-                raw_signal = []
-                with open(location + patient + "/" + file) as tsv:  # load the file
-                    for line in csv.reader(tsv, dialect="excel-tab"):  # load the required signal
-                        raw_signal.append(float(line[1]))  # ECG signal from  electrode "i"
-                        fs = 1000
-            try:
-                a = feature.Feature_Exreaction(raw_signal, fs)
-                Feature.append(a[0])
-                Labels.append(dictionary[int(patient[7:])])
-            except:
-                error += 1
+Signals = Signals[1::3]
+Labels = Labels[1::3]
 
-print(len(Feature))
-print(len(Labels))
-print(len(Feature[0]))
-print(len(Feature[10]))
-print(len(Feature[100]))
-print(len(Feature[200]))
+healthy = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Healthy control']
+ill1 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Cardiomyopathy']
+ill2 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Palpitation']
+ill3 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Hypertrophy']
+ill4 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Dysrhythmia']
+ill5 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Bundle branch block']
+ill6 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Myocarditis']
+ill7 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Valvular heart disease']
+ill8 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Heart failure (NYHA 2)']
+ill9 = [Signals[x] for x in range(len(Signals)) if Labels[x] == 'Myocardial infarction']
+
+print(list(set(Labels)))
+print(len(healthy))
+print(len(ill1), len(ill2), len(ill3), len(ill4), len(ill5), len(ill6), len(ill7), len(ill8), len(ill9))
+
+# What we wish to classify
+comp_feature = [Signals[x] for x in range(len(Signals)) if
+                Labels[x] == 'Myocardial infarction' or Labels[x] == 'Healthy control']
+comp_labels = [Labels[x] for x in range(len(Signals)) if
+               Labels[x] == 'Myocardial infarction' or Labels[x] == 'Healthy control']
+
+
+# Feature Reduction
+X = np.array(comp_feature)
+pca = PCA(n_components=3)
+pca.fit(X)
+print(pca.explained_variance_ratio_)
+print(pca.singular_values_)
+reduced = pca.fit_transform(X)
+
+
+clf = svm.SVC(kernel='linear', C=1)
+scores = cross_val_score(clf, reduced, comp_labels, cv=2)
+print(scores)
--- feature.py
+++ feature.py
@@ -7,6 +7,7 @@ from scipy import signal as sig
 import scipy.fftpack as ffttools
 from scipy import stats
 import nolds
+import csv
 
 
 def ApEn(U, m, r):
@@ -23,12 +24,14 @@ def ApEn(U, m, r):
     return abs(_phi(m + 1) - _phi(m))
 
 
+# signal = []
+# with open('patient233/s0457_re-Data.txt') as tsv:  # load the file
+#     for line in csv.reader(tsv, dialect="excel-tab"):  # load the required signal
+#         signal.append(float(line[1]))  # ECG signal from  electrode "i"
+#         fs = 1000
 
-def Feature_Exreaction(signal, fs):
-
-
+def Feature_Exteraction(signal, fs):
     # Frequency Filter
-
     h = sig.firwin(101, [4, 50], width=None, window='hamming', pass_zero=False, scale=True, fs=fs)
     # myplottools.mfreqz(h, 1, fs)  # bode plot
     ECG_filtered = sig.fftconvolve(h, signal)
@@ -36,28 +39,6 @@ def Feature_Exreaction(signal, fs):
     # R-Peak Detection
 
     rpeaks = RPeak.QRS_detection(ECG_filtered, fs)
-
-    # plt.figure()
-    # plt.plot(ECG_filtered[1:10000])
-    # for xc in rpeaks:
-    #    if xc < 10000:
-    #        plt.axvline(x=xc, color='red')
-
-    # plt.title("R-Peaks")
-    # plt.show()
-
-    # HeartBeat Extraction
-    j = 0
-    template = []
-
-    for i in rpeaks:
-        template.append(ECG_filtered[int(np.floor(i - 0.2 * fs)): int(np.floor(i + 0.4 * fs))])
-        j += 1
-    # plt.figure()
-
-    # for i in range(10):
-    #    plt.plot(template[i])
-    # plt.show()
 
     RR = []
     for i in range(len(rpeaks) - 2):
@@ -75,12 +56,13 @@ def Feature_Exreaction(signal, fs):
 
     for i in range(len(seperated)):
         mean_Feature.append(np.mean(seperated[i]))
-        std_Feature.append(np.mean(seperated[i]))
+        std_Feature.append(np.std(seperated[i]))
 
-    import numpy
+    feature1 = np.mean(mean_Feature)
+    feature2 = np.mean(std_Feature)
 
     ####### RR_Seperated Features #######
-    RR_seperated = numpy.zeros((number, N - 1))
+    RR_seperated = np.zeros((number, N - 1))
 
     for i in range(number):
         for j in range(N - 1):
@@ -89,7 +71,10 @@ def Feature_Exreaction(signal, fs):
     RR_seperated_std_Feature = []
     for i in range(number):
         RR_seperated_mean_Feature.append(np.mean(RR_seperated[i, :]))
-        RR_seperated_std_Feature.append(np.mean(RR_seperated[i, :]))
+        RR_seperated_std_Feature.append(np.std(RR_seperated[i, :]))
+
+    feature3 = np.mean(RR_seperated_std_Feature)
+    feature4 = np.mean(RR_seperated_mean_Feature)
 
     ####### pNN Feature #######
     pNN50_Feature = []
@@ -101,6 +86,10 @@ def Feature_Exreaction(signal, fs):
         pNN10_Feature.append([abs(x) for x in RR_seperated[i][:] if abs(x) > 10 * fs / 1000])
         pNN5_Feature.append([abs(x) for x in RR_seperated[i][:] if abs(x) > 5 * fs / 1000])
 
+    feature5 = len(pNN5_Feature)
+    feature6 = len(pNN10_Feature)
+    feature7 = len(pNN50_Feature)
+
     ####### Frequncy Energy Ratio Feature #######
     h_high = sig.firwin(101, [0.15, 0.4], width=None, window='hamming', pass_zero=False, scale=True, fs=1)
     h_low = sig.firwin(101, [0.04, 0.15], width=None, window='hamming', pass_zero=False, scale=True, fs=1)
@@ -110,6 +99,8 @@ def Feature_Exreaction(signal, fs):
 
     RR_Energy_Ratio_Feature = np.sum(RR_high ** 2) / np.sum(RR_low ** 2)
 
+    feature8 = RR_Energy_Ratio_Feature
+
     ####### Poincare Map Feature #######
     x = np.array(RR[0:-1])
     y = np.array(RR[1:])
@@ -118,12 +109,14 @@ def Feature_Exreaction(signal, fs):
     SD2 = np.std(np.abs(x - y + 2 * np.mean(RR)))
 
     SD_Ratio_Feature = SD1 / SD2
+    feature9 = SD_Ratio_Feature
 
     ####### Approximate Entropy Feature #######
     ApEn_Feature = []
 
     for i in range(number):
         ApEn_Feature.append(ApEn(np.array(seperated[i]), 2, 2 * np.std(seperated[i])))
+    feature10 = np.mean(ApEn_Feature)
 
     ####### Spectral Entropy Feature #######
     RR_fft = np.abs(ffttools.fft(RR))
@@ -133,8 +126,11 @@ def Feature_Exreaction(signal, fs):
     SpEn_Feature = stats.entropy(ProbDens, base=2)
     Lya_Exp_Feature = nolds.lyap_r(np.array(RR), emb_dim=2, lag=1, min_tsep=10, tau=1)
 
+    feature11 = Lya_Exp_Feature
+
     ####### Detrended Fluctuation Analysis Feature #######
     DFA_Slope_Feature = nolds.dfa(np.array(RR))
+    feature12 = DFA_Slope_Feature
 
     ####### Sequential Trend Analysis Feature #######
     DeltaRR = np.array(RR[1:]) - np.array(RR[0:-1])
@@ -147,9 +143,12 @@ def Feature_Exreaction(signal, fs):
         if DeltaRR[i] < 0 and DeltaRR[i + 1] < 0:
             NegSeqTrend_Feature += 1
 
+    feature13 = PosSeqTrend_Feature
+    feature14 = NegSeqTrend_Feature
+
     ####### Feature Vector #######
 
-    Feature = [mean_Feature + std_Feature + RR_seperated_mean_Feature + RR_seperated_std_Feature + [len(pNN5_Feature[0])] + [len(pNN10_Feature[0])] \
-              + [len(pNN50_Feature[0])] + [RR_Energy_Ratio_Feature] + [SD_Ratio_Feature] + ApEn_Feature + [Lya_Exp_Feature] \
-              + [DFA_Slope_Feature] + [PosSeqTrend_Feature] + [NegSeqTrend_Feature]]
+
+    Feature = [feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9, feature10, feature11, feature12, feature13, feature14]
+
     return Feature
